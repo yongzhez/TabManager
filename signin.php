da31a0030112987ccf64318ed0214e5d8d92de16
root <root@jason-pc>
2014-05-21 14:19:23 -0400

    can properly check for usernames and request for username again

--- signin.php
+++ signin.php
@@ -11,14 +11,27 @@ if (mysqli_connect_errno()) {
 $user = mysqli_real_escape_string($con, $_POST['user']);
 $pass = mysqli_real_escape_string($con, $_POST['pass']);
  
+$counter = 0;
+
 $result= mysqli_query($con, "SELECT * FROM  users WHERE Username='$user'");
 while($row = mysqli_fetch_array($result)) {
-  echo $row['Username'] . " " . $row['Password'];
-
-	
+  	echo "welcome " . $row['Username'];
+  	$counter ++;
 }
 
-
+if ($counter == 0){
+	echo "Invalid password or username, please try again";
+	//Check for password and username again.
+?>
+ 	<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+    Username:<input type="text" name="user">
+	Password:<input type="password" name="pass">
+	<input type="submit">
+    </form>
+<?php 
+}else{
+	echo "hello";
+}
 ?>
 
 </body>
