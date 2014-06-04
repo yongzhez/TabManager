561c8002f1862e875302c2a471ea0fee894fcf7e
yongzhez <root@jason-pc>
2014-06-03 23:27:58 -0400

    this is some more commenting

--- con.php
+++ con.php
@@ -4,7 +4,7 @@ $cred['db'] = 'learn_sql';
 $cred['user'] = 'Reader';
 $cred['pass'] = 'test';
 //global varibales used for connections.
-
+//this is the connection used.
 $con= new PDO("mysql:host= $cred['host']; dbname= $cred['db']", $cred['user'], $cred['pass']);
 
 ?>
