d59d6a65499d8c90cfb4d5b6f462e71903e4c12c
yongzhez <yongzhe.zheng1@gmail.com>
2014-07-06 16:51:17 -0400

    remodled app to be object oriented, starting from Default_page and inheriting from there

---
+++ default_page.php
@@ -0,0 +1,96 @@
+<?php
+include('wrapper.php');
+/**
+ * Class Default page
+ */
+
+class Default_page extends SmartyWrapper{
+
+	public $username;
+	public $userlist;
+	public $userid;
+
+
+	/*
+	 *
+	 */
+	function __construct(){
+		parent::__construct();
+	}
+
+	/*
+	 * @param string
+	 * assigns the username variable
+	 */
+	function assign_user($username){
+		$this->username = $username;
+	}
+
+	/*
+	 * @param array
+	 * assigns the userlist variable
+	 */
+	function assign_userlist($userlist){
+		$this->userlist = $userlist;
+	}
+
+	/*
+	 * @param array
+	 * assigns the userID variable
+	 */
+	function assign_userid($userID){
+		$this->userid = $userID;
+	}
+
+	/*
+	 * @param PDO object
+	 * Returns a simple array containing all of the names
+	 */
+	function create_userlist($con){
+		//creates a list used for matching IDs to usernames for the table
+		$result= "SELECT * FROM  users ";
+		foreach($con->query($result) as $row){
+			$this->userlist[$row['ID']]= $row['Username'];
+		}
+
+	}
+	/*
+	 * @param string
+	 * Validates that this is the correct user. Returns a boolean
+	 * in the form of 0 or 1.
+	 */
+	function  validation($pass, $con){
+
+		$counter = 0;
+		$correctness = 0;
+
+		$result= $con->prepare('SELECT * FROM  users
+			WHERE Username= ?');
+		$result->execute(array($this->username));
+		$rows = $result->fetchAll(PDO::FETCH_ASSOC);
+
+		foreach($rows as $row) {
+			if (!($row['Password'] == $pass)){
+				$correctness ++;
+				//check for password again.
+			}
+			$this->userid = $row['ID'];
+			$counter ++;
+		}
+
+		// there were no users
+		if ($counter == 0){
+			$correctness ++;
+			return $correctness;
+		}else{
+			return $correctness;
+		}
+
+	}
+	/*
+	 * Creates the table display.
+	 */
+
+}
+
+?>
---
+++ wrapper.php
@@ -0,0 +1,45 @@
+<?php
+
+require 'smarty3/Smarty.class.php';
+
+/**
+* Class SmartyWrapper
+*/
+
+class SmartyWrapper extends Smarty{
+
+	function __construct() {
+		parent::__construct();
+		$this->smartySettings();
+	}
+	function smartySettings() {
+		$this->force_compile = false;
+		$this->caching = false; #TODO: Set true for production
+		$this->merge_compiled_includes = true;
+		$this->compile_check = true; #TODO: Set false for production
+	}
+		
+	/**
+	 * @param string $file
+	 */
+	public function display_temp($file, $cache_id = null, $compile_id = null) {
+		if (!isset($compile_id)){
+            $this->compile_id = "en";
+            parent::display($file, $cache_id, $this->compile_id, NULL);
+        }else{
+            $this->compile_id = $compile_id;
+            parent::display($file, $cache_id, $this->compile_id, NULL);
+        }
+	}
+
+	/**
+	*@param string $name
+	*@param mixed $var
+	*@param bool $nochache
+	*/
+	public function assign_var($name, $var = NULL, $nocache = true){
+		parent::assign($name, $var, $nocache);
+
+	}
+}
+?>
